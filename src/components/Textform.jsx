e8f0eb66f735199c79334c2a2afa294e003014d5
joy149 <jbhwmck149@gmail.com>
2022-12-25 01:33:08 +0530

    Enhancing TetUtils App

--- src/components/Textform.jsx
+++ src/components/Textform.jsx
@@ -11,8 +11,48 @@ export default function Textform(props) {
         var newText = text;
         setText(newText.toLowerCase());
     }
+    const convertToToggleCase = () => {
+        var newText = text;
+        var words = '';
+        //console.log(newText.split(" ").filter(o=>o).map(o=>o.charAt(0).toLowerCase + o.substring(1, o.length).toUpperCase));
+        //console.log(newText);
+        newText.split(" ").forEach(word => {
+            if (word.length===1) {
+                words += word.toLowerCase() + " ";
+            }
+            else {
+                words += word.charAt(0).toLowerCase() + word.substring(1, word.length).toUpperCase() + " ";
+            }
+        })
+        //console.log(words);
+        setText(words);
+    }
+    const clearText = () => {
+        var newText = '';
+        setText(newText);
+    }
     const onChangeHandler = (event) => {
         setText(event.target.value);
+    }
+    const countVowels = (count) => {
+        var newText = text;
+        // var count = 0;
+        newText.split("").forEach(char => {
+            if (char === 'a' || char === 'A' || char === 'e' || char === 'E' || char === 'i' || char === 'I' || char === 'o' || char === 'O' || char === 'u' || char === 'U') {
+                count ++;
+            }
+        })
+        return count;
+    }
+    const countConsonants = (count) => {
+        var newText = text;
+        // var count = 0;
+        newText.split("").forEach(char => {
+            if (!(char === 'a' || char === 'A' || char === 'e' || char === 'E' || char === 'i' || char === 'I' || char === 'o' || char === 'O' || char === 'u' || char === 'U' || char === ' ')) {
+                count ++;
+            }
+        })
+        return count;
     }
 
   return (
@@ -24,12 +64,15 @@ export default function Textform(props) {
         </div>
         <button className='btn btn-primary mx-2' onClick={convertToUpperCase}>Convert to UpperCase</button>
         <button className='btn btn-primary mx-2' onClick={convertToLowerCase}>Convert to Lowercase</button>
+        <button className='btn btn-primary mx-2' onClick={convertToToggleCase}>Convert to ToggleCase</button>
+        <button className='btn btn-primary mx-2' onClick={clearText}>Clear Text</button>
     </div>
     <div className='container my-3'>
         <h2>Your Text Summary Here :</h2>
         <p>Your Text has {text.split(" ").filter(o=>o).length} words and {text.length} characters</p>
         <p>You will finish reading it in {0.005 * text.split(" ").filter(o=>o).length} minutes</p>
-        <h2>Preview</h2>
+        <p>Your Text has {countVowels(0)} vowels and {countConsonants(0)} consonants</p>
+        <h2>Preview :</h2>
         <p>{text}</p>
     </div>
     </>
